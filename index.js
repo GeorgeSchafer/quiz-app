38acbd842f438469dd2427f648fa1aaa584fd3be
George Schafer <gee.ess.mbo@gmail.com>
2019-04-23 02:10:09 -0700

    wireframe

--- index.js
+++ index.js
@@ -4,6 +4,11 @@
 function welcomePage(){
     /** The starting screen should have a button that users can click to start the quiz. */
 
+    /** 
+     * This function will welcome the user to the quiz and invite the user to continue.
+     */
+
+    console.log(`welcomePage() ran`);
 }
 
 function questionLoop(){
@@ -15,13 +20,27 @@ function questionLoop(){
         Upon submitting an answer, users should:
             receive textual feedback about their answer. If they were incorrect, they should be told the correct answer.
             be moved onto the next question (or interact with an element to move on). */
+
+
+    /**
+     * This function will iterate the user through the questions (and their answers).
+     * it will also keep score
+     */
+
+    console.log(`questionLoop() ran`);
 }
 
 function finalScore(){
     /** Users should be shown their overall score at the end of the quiz. In other words, how many questions they got right out of the total questions asked.
-        Users should be able to start a new quiz.
-    */
-}
+        Users should be able to start a new quiz.*/
+
+    /**
+     * user will be presented with their score and prompted to start a new quiz
+     * 
+     */
+
+     console.log(`finalScore() ran`)
+;}
 
 
 
